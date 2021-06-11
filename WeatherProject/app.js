5f38d1e88237e3f39c7860018faac89d7c8b9424
Sidharth Dinesh <sidharthdinesh@gmail.com>
2021-06-11 22:50:39 +0530

    Add files via upload

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -2,12 +2,22 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res){
+app.use(bodyParser.urlencoded({extended : true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=a60079d11da19cd97300749d1489d16d&units=metric";
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+
+  const query = req.body.cityName;
+  const apiKey = "a60079d11da19cd97300749d1489d16d";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
   https.get(url, function(response){
     //console.log(response);
     console.log(response.statusCode);
@@ -24,8 +34,8 @@ app.get("/", function(req,res){
       //res.send("The temperature in Mumbai is: " + temp + " degree celcius");
       //res.send("<h1>The temperature in Mumbai is: " + temp + " degree celcius</h1>");
 
-      res.write("<h1>The temperature in Mumbai is: " + temp + " degree celcius</h1>");
-      res.write("<p>The weather in Mumbai is currently: " + weatherDescp + "</p>");
+      res.write("<h1>The temperature in " + req.body.cityName + " is: " + temp + " degree celcius</h1>");
+      res.write("<p>The weather in " + req.body.cityName + " is currently: " + weatherDescp + "</p>");
 
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
